e22df9c52e423c12bd829dd9f9f6fb6102aed930
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-05 16:31:31 +0500

    repository

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-"use strict";
-
-let map = new Map([]);
+// "use strict";
 
 let input = document.querySelector(".input");
 let answerPage = document.querySelector(".answer");
@@ -8,14 +6,10 @@ let searchBox = document.querySelector(".searchBox");
 let form = document.querySelector(".form");
 let repo = document.querySelector(".repositories");
 
-input.focus();
-
 const func = async function (a) {
   let data = await fetch(`https://api.github.com/users/${a}`);
-
   let dataJson = await data.json();
-  map = Object.entries(dataJson);
-  console.log(dataJson);
+  // console.log(dataJson);
 
   answerPage.innerHTML = `
   <div class="answer--left flex">
@@ -38,17 +32,106 @@ const func = async function (a) {
   </div>`;
 };
 
+function getRepHtml(a) {
+  a.forEach((el) => {
+    let html = `<div class="repositories flex">
+        <div class="repoTitle">
+          <h1>Latest Repos</h1>
+        </div>
+
+        <div class="latestRepos flex">
+          <p class="repoName">${el.name}</p>
+
+          <div class="flex">
+            <div class="box1"></div>
+            <div class="box1" >Watchers:1</div>
+            <div class="box1" >Forks:0</div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>`;
+
+    answerPage.insertAdjacentHTML("afterend", html);
+  });
+}
+
 let search = function () {
-  form.addEventListener("input", function (e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let val = input.value;
-    func(val);
+    func(input.value);
+    getRepos(input.value);
 
-    answerPage.innerHTML = "";
+    // answerPage.innerHTML = "";
   });
 };
 
+function getRepos(res) {
+  fetch(`https://api.github.com/users/${res}/repos`)
+    .then((bir) => {
+      return bir.json();
+    })
+    .then((ikki) => {
+      console.log(ikki);
+      getRepHtml(ikki);
+    });
+}
+
 search();
 
-// fetch(https://api.github.com/users/${user}?client_id=65b44d46d520be1f19c7&client_secret=7287ef205413001a79b30f0fbcc04416153ef797);
+// // fetch(https://api.github.com/users/${user}?client_id=65b44d46d520be1f19c7&client_secret=7287ef205413001a79b30f0fbcc04416153ef797);
+
+// let input = document.querySelector(".input");
+// let answerPage = document.querySelector(".answer");
+// let searchBox = document.querySelector(".searchBox");
+// let form = document.querySelector(".form");
+// let repo = document.querySelector(".repositories");
+
+// function getUrl(user) {
+//   // fetch(
+//   //   `https://api.github.com/users/${user}?client_id=8eace0a7f23412d4b735&client_secret=9be5fdeba5f89a420460a33d33106ec61f5b9758)`
+//   // )
+//   fetch(`https://api.github.com/users/${user}`)
+//     .then((one) => {
+//       return one.json();
+//     })
+//     .then((two) => {
+//       console.log(two);
+//       htmlFile(two);
+//       console.log(two);
+//     });
+// }
+
+// function htmlFile(arr) {
+//   answerPage.insertAdjacentHTML(
+//     "beforeend",
+//     `
+//   <div class="answer--left flex">
+//     <img src="${arr.avatar_url}" alt="">
+//     <a href="${arr.html_url}" class="btn flex">View Profile</a>
+//   </div>
+//   <div class="answer--right">
+//   <div class="answer--right--top flex">
+//     <div class="repo box1"><p>Public Repos:${arr.public_repos}</p></div>
+//     <div class="gists box1"><p>Public Gists:${arr.public_gists}</p></div>
+//     <div class="follower box1"><p>Followers:${arr.followers}</p></div>
+//     <div class="following box1 "><p>Following:${arr.following}</p></div>
+//   </div>
+//   <div class="answer--right--bottom">
+//     <p class="box2">Company:${arr.company}</p>
+//     <p class="box2">Website/Blog:${arr.events_url}</p>
+//     <p class="box2">Location:${arr.location}</p>
+//     <p class="box2">Member since:${arr.updated_at}</p>
+//   </div>
+//   </div>`
+//   );
+// }
+
+// document.addEventListener("keydown", function (e) {
+//   if (e.key == "Enter") {
+//     getUrl(input.value);
+//     input.value = "";
+//   }
+// });
